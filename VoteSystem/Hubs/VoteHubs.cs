dcdfa57c09d3d1312f3a6e173539b70f998fb71b
Jose Davila <44849949+codegroupsolutions@users.noreply.github.com>
2024-11-02 11:21:34 -0400

    add project

---
+++ VoteSystem/Hubs/VoteHubs.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace VoteSystem.Hubs
+{
+    public class VoteHubs : Hub
+    {
+        public async Task SendVote(string username, int vote)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", username, vote);
+        }
+    }
+}
